6a73094bc0af619ce2058f9b4f91a592d1fab782
Narendra Kumar <narendra.kumar@firstfuel.com>
2017-12-24 16:46:01 +0530

    Fixed home page

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,4 +1,6 @@
 /* after changing this file run 'npm run webpack:build' */
 /* tslint:disable */
 import '../content/scss/vendor.scss';
+import '../content/js/move-top.js';
+
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
